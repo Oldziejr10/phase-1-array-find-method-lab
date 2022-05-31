caab33c47425e3fe21fdbded560ac76903d05c21
Rafal Oldziej <rafal.oldziej1@gmail.com>
2022-05-30 22:20:15 -0400

    commit tree 82d756aa0ec660b01667419b7b30dba957b6ad99

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(records) {
+    let foundRecord = records.find(function(element) {
+        return element.result === `W` 
+    } ) 
+    if (foundRecord ) {
+        return foundRecord.year 
+    }
+
+}
